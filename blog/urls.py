0c9a42a5c8ddbe4148c1ef48576e289847fa6aad
Yabe Tatsuya <yabe.tatsuya0626@gmail.com>
2018-11-23 22:50:58 +0900

    Add View

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from django.conf.urls import url 
+from django.conf.urls import url
 from . import views
 
 urlpatterns=[
     url('',views.post_list,name='post_list'),
+    url(r'^post/<int:pk>/',views.post_detail,name='post_detail'),
 ]
