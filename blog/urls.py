defbf2e8e247d428895d5db857d62b53b92e4352
Yabe Tatsuya <yabe.tatsuya0626@gmail.com>
2018-11-27 22:19:23 +0900

    修了

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns=[
     url('',views.post_list,name='post_list'),
-    url(r'^post/<int:pk>/',views.post_detail,name='post_detail'),
+    url('post/<int:pk>/',views.post_detail,name='post_detail'),
 ]
